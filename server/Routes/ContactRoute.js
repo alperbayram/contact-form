4055b2de19a4f509f604afde2fe9bdb00eb2bbce
alperbayram <alperbayrm@hotmail.com>
2022-02-07 17:38:39 +0300

    deletepackage

--- server/Routes/ContactRoute.js
+++ server/Routes/ContactRoute.js
@@ -3,7 +3,6 @@ const ContactController = require("../Controller/ContactController");
 
 const router = express.Router();
 
-router.route("/").get(ContactController.getContact);
 router.route("/").post(ContactController.sendEmail);
 
 module.exports = router;
